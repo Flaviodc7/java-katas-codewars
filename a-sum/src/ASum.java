345b455cbab928c7a7592198f3ebd824836eb66f
Flavio Coscarella <flaviodc@live.com.ar>
2023-04-01 11:13:58 -0300

    Kata Build a pile of Cubes

---
+++ a-sum/src/ASum.java
@@ -0,0 +1,21 @@
+public class ASum {
+
+    public static long findNb(long m) {
+        long result = 0;
+        long evaluate_number = 0;
+        do {
+            result++;
+            evaluate_number = ((long) Math.pow(result, 2) * (long) Math.pow(result + 1, 2)) / 4;
+        } while (evaluate_number < m);
+        if (evaluate_number == m){
+            return result;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args){
+        long result = findNb(1775537264590751376L);
+        System.out.println(result);
+    }
+}
