48081847b5eba59adee9c328f5c351c504dd3d24
Flavio Coscarella <flaviodc@live.com.ar>
2023-04-01 20:59:31 -0300

    Kata Small enough? - Beginner

---
+++ Kata/src/Kata.java
@@ -0,0 +1,16 @@
+public class Kata
+{
+    public static boolean smallEnough(int[] a, int limit){
+        for (int j : a) {
+            if (j > limit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        boolean result = smallEnough(new int[] { 78, 117, 110, 99, 104, 117, 107, 115 }, 100);
+        System.out.println(result);
+    }
+}
